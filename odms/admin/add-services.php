6394e0498c81170c7821355e9e8648f31b406eba
Bagasszz <bagasadhinugraha6@gmail.com>
2025-05-16 08:39:37 +0700

    first project dj

---
+++ odms/admin/add-services.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+
+if (strlen($_SESSION['odmsaid']) == 0) {
+    header('location:logout.php');
+} else {
+    if (isset($_POST['submit'])) {
+        $sername = $_POST['sername'];
+        $serdes = $_POST['serdes'];
+        $serprice = $_POST['serprice'];
+
+        $sql = "INSERT INTO tblservice(ServiceName, SerDes, ServicePrice) VALUES (:sername, :serdes, :serprice)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':sername', $sername, PDO::PARAM_STR);
+        $query->bindParam(':serdes', $serdes, PDO::PARAM_STR);
+        $query->bindParam(':serprice', $serprice, PDO::PARAM_STR);
+        $query->execute();
+
+        $LastInsertId = $dbh->lastInsertId();
+        if ($LastInsertId > 0) {
+            echo '<script>alert("Services has been added.")</script>';
+            echo "<script>window.location.href ='add-services.php'</script>";
+        } else {
+            echo '<script>alert("Something went wrong. Please try again.")</script>';
+        }
+    }
+?>
+<!doctype html>
+<html lang="en" class="no-focus">
+<head>
+    <title>Online DJ Management System - Add Services</title>
+    <link rel="stylesheet" id="css-main" href="assets/css/codebase.min.css">
+</head>
+<body>
+    <div id="page-container" class="sidebar-o sidebar-inverse side-scroll page-header-fixed main-content-narrow">
+        
+        <?php include_once('includes/sidebar.php'); ?>
+        <?php include_once('includes/header.php'); ?>
+
+        <!-- Main Container -->
+        <main id="main-container">
+            <!-- Page Content -->
+            <div class="content">
+                <h2 class="content-heading">Add Services</h2>
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- Bootstrap Register -->
+                        <div class="block block-themed">
+                            <div class="block-header bg-gd-emerald">
+                                <h3 class="block-title">Add Services</h3>
+                                <div class="block-options">
+                                    <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
+                                        <i class="si si-refresh"></i>
+                                    </button>
+                                    <button type="button" class="btn-block-option" data-toggle="block-option" data-action="content_toggle"></button>
+                                </div>
+                            </div>
+                            <div class="block-content">
+                                <form method="post">
+                                    <div class="form-group row">
+                                        <label class="col-12">Service Name:</label>
+                                        <div class="col-12">
+                                            <input type="text" class="form-control" name="sername" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-12">Service Description:</label>
+                                        <div class="col-12">
+                                            <textarea class="form-control" name="serdes" required></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-12">Service Price:</label>
+                                        <div class="col-12">
+                                            <input type="text" class="form-control" name="serprice" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <button type="submit" class="btn btn-alt-success" name="submit">
+                                                <i class="fa fa-plus mr-5"></i> Add
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- END Bootstrap Register -->
+                    </div>
+                </div>
+            </div>
+            <!-- END Page Content -->
+        </main>
+        <!-- END Main Container -->
+
+        <?php include_once('includes/footer.php'); ?>
+    </div>
+
+    <!-- Codebase Core JS -->
+    <script src="assets/js/core/jquery.min.js"></script>
+    <script src="assets/js/core/popper.min.js"></script>
+    <script src="assets/js/core/bootstrap.min.js"></script>
+    <script src="assets/js/core/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/core/jquery.scrollLock.min.js"></script>
+    <script src="assets/js/core/jquery.appear.min.js"></script>
+    <script src="assets/js/core/jquery.countTo.min.js"></script>
+    <script src="assets/js/core/js.cookie.min.js"></script>
+    <script src="assets/js/codebase.js"></script>
+</body>
+</html>
+<?php } ?>
